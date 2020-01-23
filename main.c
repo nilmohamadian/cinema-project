40dd3d86736fd474786127a0b228b07068a9fb26
niloufar <fatemeh2000.mohamadian@gmail.com>
2020-01-23 18:18:18 +0330

    file functions

--- main.c
+++ main.c
@@ -32,3 +32,67 @@ typedef struct Salon
     Sanse sanse[8];
     int numChair;
 } Salon;
+void fileReader(Salon x[],int n)
+{
+    int i,j;
+    FILE *p;
+    p=fopen("Salon.txt","r");
+    for(i=0;i<n;i++)
+    {
+        fscanf(p,"%p",&x[i].name);
+        for(j=0;j<8;j++)
+        {
+            fscanf(p,"%d",&x[i].sanse[j].numSanse);
+            fscanf(p,"%p",&x[i].sanse[j].salonNameOwner);
+            fscanf(p,"%d",&x[i].sanse[j].day);
+            fscanf(p,"%f",&x[i].sanse[j].startTime);
+            fscanf(p,"%f",&x[i].sanse[j].endTime);
+            fscanf(p,"%d",&x[i].sanse[j].assigned);
+            fscanf(p,"%p",&x[i].sanse[j].film.filmName);
+            fscanf(p,"%f",&x[i].sanse[j].film.length);
+            fscanf(p,"%p",&x[i].sanse[j].film.actors);
+            fscanf(p,"%p",&x[i].sanse[j].film.summary);
+            int k=0;
+            for(k=0;k<10;k++)
+            {
+                int l;
+                for(l=0;l<5;l++)
+                    fscanf(p,"%d",&x[i].sanse[j].chair[k][l]);
+            }
+        }
+        fscanf(p,"%p",&x[i].numChair);
+    }
+    fclose(p);
+}
+void fileWriter (Salon x[],int n)
+{
+    int i,j;
+    FILE *p;
+    p=fopen("Salon.txt","w");
+    for(i=0;i<n;i++)
+    {
+        fprintf(p,"%p ",x[i].name);
+        for(j=0;j<8;j++)
+        {
+            fprintf(p,"%d ",x[i].sanse[j].numSanse);
+            fprintf(p,"%p ",x[i].sanse[j].salonNameOwner);
+            fprintf(p,"%d ",x[i].sanse[j].day);
+            fprintf(p,"%f ",x[i].sanse[j].startTime);
+            fprintf(p,"%f ",x[i].sanse[j].endTime);
+            fprintf(p,"%d ",x[i].sanse[j].assigned);
+            fprintf(p,"%p ",x[i].sanse[j].film.filmName);
+            fprintf(p,"%f ",x[i].sanse[j].film.length);
+            fprintf(p,"%p ",x[i].sanse[j].film.actors);
+            fprintf(p,"%p ",x[i].sanse[j].film.summary);
+            int k=0;
+            for(k=0;k<10;k++)
+            {
+                int l;
+                for(l=0;l<5;l++)
+                    fprintf(p,"%d ",x[i].sanse[j].chair[k][l]);
+            }
+        }
+        fprintf(p,"%p ",x[i].numChair);
+    }
+    fclose(p);
+}
